ab9cc1774ba89c2aabb71d82aeea0f8f33c7503b
Toby Pinder <toby.pinder@smithelectric.com>
2013-04-22 10:30:22 +0100

    7.4.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
 
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   
   def new
+    @user = User.new
   end
+
+  private
+    #Mass assignment protection.
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
