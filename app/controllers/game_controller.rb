0dd1848c2933311ad957a922f7295f19a05853b4
Toby Pinder <gigitrix@gmail.com>
2013-04-28 11:41:54 +0100

    Hopefully fixed login funkiness

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,7 +3,7 @@ class GameController < ApplicationController
   before_filter :authenticate_user! 
   respond_to :json, except: [:index]
   def index
-     current_user.init_defaults
+     current_user.player_status_check
   end
 
   def player_info
