786cd365441a8f4a251d1bcc22acee552d08fe2a
Toby Pinder <gigitrix@gmail.com>
2013-04-28 11:19:13 +0100

    ???

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,7 +2,9 @@
 # The position model exists as a common framework for tile coords in case I need to add
 # floors, other maps etc. Currently just [x,y]
 #
-
 class Position < ActiveRecord::Base
   has_many :users
+
+  
+
 end
--- app/models/queued_item.rb
+++ app/models/queued_item.rb
@@ -1,3 +1,24 @@
 class QueuedItem < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+
+  ##process the event
+  def process
+    case self.action
+    when 'move_rest'
+      
+    when 'move_left'
+      
+    when 'move_right'
+      
+    when 'move_up'
+      
+    when 'move_down'  
+
+    end 
+  end  
+  ##processes the event, destroying it!
+  def process!
+    self.process
+  end
+
 end
